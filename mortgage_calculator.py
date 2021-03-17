2cc5dbb6af4744dfcf11d40b31216dbf4fcdfec7
nazcat <nazli.uremek@gmail.com>
2021-03-17 19:20:56 -0400

    Update mortgage_calculator.py

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -1,52 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
 monthly_compound = lambda x , r , t : x * (1 + r)**t
-    
 constant_monthly_compound = lambda x , r , t : np.sum( [ monthly_compound(x,r,j) for j in range(1,t+1) ] )
 
 def compute_total_value_from_deposits( x0 , xmonthly , annual_percentage , n_months ):
     
-    return monthly_compound( x0 , annual_percentage/100./12. , n_months ) + \
+        return monthly_compound( x0 , annual_percentage/100./12. , n_months ) + \
         constant_monthly_compound( xmonthly , annual_percentage/100./12. , n_months )
     
-
-
     
 def main( x0 , xmonthly ):
-    
     annual_rates = [ 0 , 4 , 8 , 12 , 16 , 20 ]
-    
     months = np.arange( 1 , 48 )
-    
     balance = []
     
     for (i,ri) in enumerate(annual_rates):
         
-        balance.append( [ compute_total_value_from_deposits( x0 , xmonthly , ri , tj ) for tj in months ] )
-                
+        balance.append( [ compute_total_value_from_deposits( x0 , xmonthly , ri , tj ) for tj in months ] )   
         if ri==0:
             plt.plot( months , balance[i] , 'r--' )
         else:
             plt.plot( months , balance[i] , 'b' )
     
     plt.grid()
-    
     plt.legend( [ str(ri) + '%' for ri in annual_rates ] )
 
 
 def example_buy_a_house( x0 , xmonthly , annual_rate_mean , annual_rate_std , xmortgage , nbuyhouse ):
     
     random_r = lambda : np.random.normal( annual_rate_mean , annual_rate_std )
-    
     nmonths = 12*10
-    
     months = np.arange( 1 , nmonths+1 )
-    
     months_mortgage = np.arange( nbuyhouse , nmonths )
-    
     value_deposits = np.array( [ compute_total_value_from_deposits( x0 , xmonthly , random_r() , tj ) for tj in months ] )
         
     net_value = np.zeros_like( months )
@@ -62,8 +48,6 @@ def example_buy_a_house( x0 , xmonthly , annual_rate_mean , annual_rate_std , xm
     else:
         net_value[months_mortgage[0]:]  = [ compute_total_value_from_deposits( \
                                                 value_deposits[months_mortgage[0]] , xmonthly-xmortgage , random_r() , tj-months_mortgage[0] ) for tj in months_mortgage ]
-
-
     return value_deposits , net_value
 
 
@@ -75,34 +59,27 @@ def mc_sample_houses( x0 , xmonthly , annual_rate_mean , annual_rate_std , xmort
     for i in range( mc_samples ):
         
         xmort = np.random.normal( xmortgage_mean , xmortgage_std )
-        
         _ , net_value_i = example_buy_a_house( x0 , xmonthly , annual_rate_mean , annual_rate_std , xmort , \
                                                np.random.choice( np.arange( nbuyhouse_min , nbuyhouse_max + 1 ) ) )
-        
         mc_value.append( net_value_i )
-    
     return np.array( mc_value )
 
 
 
 
 def example_buy_a_house_2( worth_t0 , monthly_income_after_tax_t0 , monthly_expenses_not_rent , \
-                           rent_before_house , investments_apr , income_apr , xmortgage , year_buy_house ):
+                           rent_before_house , investments_apr , income_apr , xmortgage , year_buy_house ):  
     
     nmonths = 12*10
-    
     months = np.arange( 1 , nmonths+1 )
-    
     months_mortgage = np.arange( year_buy_house , nmonths )
-        
     xmonthly  = monthly_income_after_tax_t0 - ( monthly_expenses_not_rent + rent_before_house )
-    
-    value_deposits = np.array( [ compute_total_value_from_deposits( worth_t0 , xmonthly , investments_apr() , tj ) for tj in months ] )
-        
+    value_deposits = np.array( [ compute_total_value_from_deposits( worth_t0 , xmonthly , investments_apr() , tj ) for tj in months ] ) 
     net_value = np.zeros_like( months )
     net_value[0:months_mortgage[0]] = value_deposits[0:months_mortgage[0]]
     
     if ( monthly_income_after_tax_t0 - ( monthly_expenses_not_rent + xmortgage ) < 0 ):
+        
         net_value[months_mortgage[0]:]  = [ monthly_compound( \
                                                 value_deposits[months_mortgage[0]] , investments_apr()/100./12. , tj-months_mortgage[0] ) \
                                                 for tj in months_mortgage ]
@@ -110,6 +87,7 @@ def example_buy_a_house_2( worth_t0 , monthly_income_after_tax_t0 , monthly_expe
                                                 ( monthly_expenses_not_rent + xmortgage ) - monthly_income_after_tax_t0 , \
                                                 investments_apr()/100./12. , tj-months_mortgage[0] ) \
                                                 for tj in months_mortgage[1:] ] )
+        
     else:
         net_value[months_mortgage[0]:]  = [ compute_total_value_from_deposits( \
                                                 value_deposits[months_mortgage[0]] , \
@@ -123,14 +101,12 @@ def mc_sample_houses_2( worth_t0 , monthly_income_after_tax_t0 , monthly_expense
                         rent_before_house , yearly_investments_apr , yearly_income_apr , monthly_mortgage , year_buy_house , mc_samples ):
     
     mc_value = []
-    
     for i in range( mc_samples ):
-    
+        
         _ , net_value_i = example_buy_a_house_2( worth_t0 , monthly_income_after_tax_t0 , monthly_expenses_not_rent , \
                                                  rent_before_house , yearly_investments_apr , yearly_income_apr , monthly_mortgage() , year_buy_house() )
-        
         mc_value.append( net_value_i )
-    
+        
     return np.array( mc_value )
 
 
@@ -139,7 +115,6 @@ def mc_sample_houses_2( worth_t0 , monthly_income_after_tax_t0 , monthly_expense
 def compute_total_house_price( monthly_mortgage_mean , apr_house , nyears ):
     
     total_deposits = monthly_mortgage_mean * np.sum( [ ( 1 + apr_house/12./100. )**j for j in range(0,nyears*12+1) ] )
-    
     return total_deposits / (1 + apr_house/12./100.)**(nyears*12)
 
 
